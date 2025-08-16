deefd837e1ad1a33ed30b1622ba80db3d47bfdcc
Mario Hideki Murata <106937976+muratahideki@users.noreply.github.com>
2025-08-15 22:14:05 -0300

    Create Transformation-dna.py

---
+++ Transformation-dna.py
@@ -0,0 +1,17 @@
+molde2dna = sequence
+#duplication
+def duplication(molde2dna):
+    molde2dna.replace("A","T")
+    molde2dna.replace("T","A")
+    molde2dna.replace("C","G")
+    molde2dna.replace("G","C")
+
+    return molde2dna
+
+#transcription
+molde2rna = sequence 
+def transcription(molde2rna):
+    molde2rna.replace("A","U")
+    molde2rna.replace("T","A")
+    molde2rna.replace("C","G")
+    molde2rna.replace("G","C")
