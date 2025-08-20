01df01929340a180153eb6aefef0372c0c66c10a
Mario Hideki Murata <106937976+muratahideki@users.noreply.github.com>
2025-08-19 23:57:07 -0300

    Update Transformation_dna.py

--- Transformation_dna.py
+++ Transformation_dna.py
@@ -1,17 +1,19 @@
 # molde_dna = sequence
 #duplication
 
-def duplication(molde_dna):
+def duplication(molde2dna):
     sequencia = {'A':'T', 'T':'A', 'C':'G', 'G':'C'}
     fita_duplicada = ""
-    for base in molde_dna:
+    for base in molde2dna:
         fita_duplicada += sequencia[base]
     return fita_duplicada
 
 #transcription
 # molde2rna = sequence 
+
 def transcription(molde2rna):
-    molde2rna.replace("A","U")
-    molde2rna.replace("T","A")
-    molde2rna.replace("C","G")
-    molde2rna.replace("G","C")
+    sequencia = {'A':'U', 'T':'A', 'C':'G', 'G':'C' }
+    fita_rna = ""
+    for base in molde2rna:
+        fita_rna += sequencia[base]
+    return fita_rna
