97747ad5d1a3ac31a008a293006610b611c2013b
Mario Hideki Murata <106937976+muratahideki@users.noreply.github.com>
2026-01-06 22:30:41 -0300

    Update pipeline.sh

--- automation/pipeline.sh
+++ automation/pipeline.sh
@@ -21,9 +21,6 @@ QC_DIR="results/fastqc"
 ALIGN_DIR="results/aligned"
 COUNT_DIR="results/counts"
 
-GENOME_INDEX="reference/hisat2/genome_index"
-GTF="reference/annotation.gtf"
-
 # ===============================
 # ATIVAR CONDA
 # ===============================
@@ -36,8 +33,11 @@ conda activate $ENV_NAME
 mkdir -p \
   $RAW_DIR_IPT $RAW_DIR_NIPT \
   $RESULTS_IPT_SPLIT_FILE $RESULTS_NIPT_SPLIT_FILE \
-  $TRIM_DIR_IPT $TRIM_DIR_NIPT\
-  $QC_DIR $ALIGN_DIR $COUNT_DIR
+  $TRIM_DIR_IPT $TRIM_DIR_NIPT \
+  $ALIGN_DIR $COUNT_DIR \
+  $GENOME_INDEX
+
+mkdir -p hisat2
 
 echo "Diretórios criados"
 
@@ -136,3 +136,72 @@ echo "Removendo arquivos unpaired..."
 rm -f "$TRIM_DIR_IPT"/*_unpaired.fastq.gz
 rm -f "$TRIM_DIR_NIPT"/*_unpaired.fastq.gz
 
+# ===============================
+# Mapeamento-  Hisat2
+# ===============================
+
+GENOME_INDEX="automation/hisat2/genome_index"
+GTF="automation/annotation.gtf"
+
+# download do genoma
+curl --cookie jgi_session=/api/sessions/72c8fbec4d695cdee2d873b55b50b2e2 --output download.20260106.211503.zip -d "{\"ids\":{\"Phytozome-771\":{\"file_ids\":[\"642da257ed041a78f31f89b2\",\"642da256ed041a78f31f899c\"],\"top_hit\":\"642da256ed041a78f31f89a4\"}},\"api_version\":\"2\"}" -H "Content-Type: application/json" https://files-download.jgi.doe.gov/filedownload/
+unzip download.20260106.211503.zip
+
+arquivos="Phytozome/PhytozomeV13/SofficinarumxspontaneumR570/v2.1"
+
+pasta_genoma="$arquivos"/assembly
+genoma="$pasta_genoma"/SofficinarumxspontaneumR570_771_v2.0.softmasked.fa.gz
+gunzip $genoma
+genoma="${genoma%.gz}"
+
+#download anotation
+curl --cookie jgi_session=/api/sessions/72c8fbec4d695cdee2d873b55b50b2e2 --output download.20260106.220955.zip -d "{\"ids\":{\"Phytozome-771\":{\"file_ids\":[\"642da256ed041a78f31f899a\"],\"top_hit\":\"642da256ed041a78f31f89a4\"}},\"api_version\":\"2\"}" -H "Content-Type: application/json" https://files-download.jgi.doe.gov/filedownload/
+unzip download.20260106.220955.zip
+pasta_annotation="$arquivos"/annotation
+annotation="$pasta_annotation"/SofficinarumxspontaneumR570_771_v2.1.gene.gff3.gz
+gunzip $annotation
+annotation="${annotation%.gz}"
+
+# build 
+hisat2-build \
+-p $THREADS \
+"$genoma" \
+$GENOME_INDEX
+
+# ===============================
+# aLINHAMENTO ipt
+# ===============================
+echo "🧬 Alinhando IPT com HISAT2"
+
+mkdir -p "$ALIGN_DIR/IPT"
+
+for R1 in "$TRIM_DIR_IPT"/*_1_paired.fastq.gz; do
+  SAMPLE=$(basename "$R1" _1_paired.fastq.gz)
+  R2="$TRIM_DIR_IPT/${SAMPLE}_2_paired.fastq.gz"
+
+  hisat2 \
+    -p $THREADS \
+    -x $GENOME_INDEX \
+    -1 "$R1" \
+    -2 "$R2" \
+  | samtools view -@ $THREADS -bS - > "$ALIGN_DIR/IPT/${SAMPLE}.bam"
+
+done
+
+
+echo "🧬 Alinhando NIPT com HISAT2"
+
+mkdir -p "$ALIGN_DIR/NIPT"
+
+for R1 in "$TRIM_DIR_NIPT"/*_1_paired.fastq.gz; do 
+  SAMPLE=$(basename "$R1" _1_paired.fastq.gz)
+  R2="$TRIM_DIR_NIPT/${SAMPLE}_2_paired.fastq.gz"
+
+  hisat2 \
+    -p $THREADS \
+    -x $GENOME_INDEX \
+    -1 "$R1" \
+    -2 "$R2" \
+  | samtools view -@ $THREADS -bS - > "$ALIGN_DIR/NIPT/${SAMPLE}.bam"
+
+done
